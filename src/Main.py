641734da105f578ff64fb496eec1aba28d757afa
Thomas Cadart <thomas.cadart@utbm.fr>
2024-12-17 10:25:15 +0100

    cleaning and optimizing

--- src/Main.py
+++ src/Main.py
@@ -1,7 +1,7 @@
 from src.settings import *
 from src.Lattice import *
 
-#Generate data from lattice
+# Generate data from lattice
 if Lattice_Type != 1000:
     lattice = Lattice(cell_size_X, cell_size_Y, cell_size_Z, number_cell_X, number_cell_Y, number_cell_Z, Lattice_Type,
                       Radius, gradRadiusProperty, gradDimProperty, gradMatProperty, MethodSim, uncertaintyNodeSD)
